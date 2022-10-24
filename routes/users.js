38a107745a6e75fa17656e8a8366c3fdd231338f
Viraj <virajmalaka94@gmail.com>
2022-10-24 20:12:44 +0530

    updated userController.js

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,19 @@
 import express from "express";
+import { createUser, deleteUser, getAllUsers, getUser, updateUser } from "../controllers/UserController.js";
+import User from "../models/User.js";
+import { createError } from "./utils/error.js";
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-    res.send("this is user")
-})
+//create
+router.post("/", createUser)
+//update
+router.put("/:id", updateUser)
+//delete
+router.delete("/:id", deleteUser)
+//get
+router.get("/:id", getUser)  
+//get all
+router.get("/", getAllUsers)
 
 export default router
